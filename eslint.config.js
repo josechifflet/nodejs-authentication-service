36c5123226930b22e16b30780326546de1aab8ea
Jose Pedro Chifflet <josechifflet98@gmail.com>
2024-07-04 17:33:09 -0300

    chore: Update eslint and npm dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
